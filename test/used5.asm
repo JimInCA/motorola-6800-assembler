5fe73a5dcf075808219ce0021e682a2f39988fb2
Jim <jnewman@LeapMotion.com>
2017-05-18 07:53:11 -0700

    Enhanced Test Cases

--- test/used5.asm
+++ test/used5.asm
@@ -1,6 +1,13 @@
 *
+*	    Copined from:
+*	    MEK6802D5 Microcomputer Evaluation Board User's Manual
+*	    Page 3-8
 *
-*        OPT     c,cre
+*	    Assemble with the following command:
+*		as0 use_d5.asm -I cre c s
+*
+        NAM     USED5
+        OPT     c,cre
         org     $0
 disbuf  equ     $e41d
 diddle  equ     $f0a2
